b127a439be487d6497c4a7b614208ac5de5b4599
riq0007 <henriquesaideoliveira2006@gmail.com>
2025-09-02 21:37:11 -0300

    Opções 1, 2 e 3

--- telaListarPizzas.js
+++ telaListarPizzas.js
@@ -1,3 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline = require("readline-sync");
+function menu() {
+    console.log("------Menu Pizzaria------");
+    console.log("1 - inserir pizzas");
+    console.log("2 - Listar pizzas");
+    console.log("3 - Procurar pizza por id");
+    console.log("4 - Sair");
+    var option = readline.questionInt("Escolha uma op\u00E7\u00E3o: \"");
+    switch (option) {
+        case 1:
+            var id = readline.questionInt("Id da pizza:");
+            var name_1 = readline.questionInt("Nome da pizza:");
+            var description = readline.questionInt("Descri\u00E7\u00E3o da pizza:");
+            var stock = readline.questionInt("Stoque da pizza (quantas tem)");
+            break;
+    }
+}
 var pizzas = [];
 function addPizza(pizza) {
     pizzas.push(pizza);
--- telaListarPizzas.ts
+++ telaListarPizzas.ts
@@ -1,24 +1,4 @@
 import * as readline from 'readline-sync';
-function menu(): void{
-    console.log(`------Menu Pizzaria------`)
-    console.log(`1 - inserir pizzas`)
-    console.log(`2 - Listar pizzas`)
-    console.log(`3 - Procurar pizza por id`)
-    console.log(`4 - Sair`)
-    
-
-    const option = readline.questionInt(`Escolha uma opção: "`);
-
-    switch(option){
-        case 1:
-            const id = readline.questionInt(`Id da pizza:`);
-            const name = readline.questionInt(`Nome da pizza:`);
-            const description = readline.questionInt(`Descrição da pizza:`);
-            const stock = readline.questionInt(`Stoque da pizza (quantas tem)`);
-            break;
-    }
-
-}
 interface Pizza {
     id: number;
     name: string;
@@ -33,6 +13,50 @@ function addPizza(pizza: Pizza){
 function listPizzas ():void{
     pizzas.forEach(u => console.log(`${u.id} ${u.name} ${u.description} ${u.stock}`));
 }
+function menu(): void{
+    console.log(`------Menu Pizzaria------`)
+    console.log(`1 - inserir pizzas`)
+    console.log(`2 - Listar pizzas`)
+    console.log(`3 - Procurar pizza por id`)
+    console.log(`4 - Sair`)
+    
+
+    const option: number = readline.questionInt(`Escolha uma opção: "`) || 0;
+
+    switch(option){
+        case 1:
+            const id: number = readline.questionInt(`Id da pizza:`) || 0;
+            const name = readline.question(`Nome da pizza:`);
+            const description = readline.question(`Descrição da pizza:`);
+            const stock: boolean = readline.keyInYN(`Stoque da pizza (quantas tem):`) || false;
+            addPizza({id, name , description , stock})
+            break;
+            
+            case 2:
+                listPizzas();
+                break;
+            
+            case 3:
+                let encontrada = false;
+    
+                const idEncontrarPizza = readline.questionInt(`Digite o ID da pizza que você quer encontrar:`)
+                for(let i = 0; i < pizzas.length; i++){
+                    if(pizzas[i].id == idEncontrarPizza){
+                        console.log(`${pizzas[i].id} - ${pizzas[i].name} - ${pizzas[i].stock}`)
+                        encontrada = true;
+                        break;
+                    }
+                    
+                }
+                    if(!encontrada){
+                        console.log(`Pizza não foi encontrada`)
+                        
+                }
+    
+        
+        }   
+    }
+
 addPizza({id: 1, name: "Marguerita", description: "Pizza com molho de tomate", stock: true})
 
 listPizzas();
